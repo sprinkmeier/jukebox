e6c15d27d7534afa61a58b77e8d00282621476c3
Thomas Sprinkmeier <thomas.sprinkmeier@gmail.com>
2013-03-23 16:31:02 +1030

    add control messages and status output

--- jukebox.py
+++ jukebox.py
@@ -1,11 +1,79 @@
 #!/usr/bin/env python
 
-import socket
-import sys
+
+import glob
+import json
 import os
+import select
+import socket
+import subprocess
+import sys
+import time
 import traceback
 
+DevNull = open('/dev/null','rw')
 PORT = 55555
+
+Q = []
+
+p = None
+filename = None
+
+def play(filename):
+    p = subprocess.Popen(('play', filename),
+                         stdout = DevNull,
+                         stdin  = DevNull,
+                         stderr = DevNull)
+
+    return p
+
+def playGlob(filenameGlob):
+    filenames = glob.glob(filenameGlob)
+    if (len(filenames) == 1):
+        filename = filenames[0]
+        return (filename, play(filename))
+    return None
+
+class AtomicFile(object):
+    def __init__(self, filename, mode = 'w'):
+        self.filename = filename
+        self.new = filename + ".new"
+        self.w = open(self.new, mode)
+
+    def __enter__(self, *extra):
+        #print(extra)
+        return self.w
+
+    def __exit__(self, *extra):
+        #print(extra)
+        self.w.close()
+        os.rename(self.new, self.filename)
+
+def process(data, address):
+    global p
+    global Q
+
+
+    if not data: return
+
+    print(data, address)
+
+    if (data == 'Stop'):
+        if p:
+            p.kill()
+        return
+    if (data == 'Flush'):
+        Q = []
+        return
+    if (data == 'Shutdown'):
+        return
+    if (data == 'Play'):
+        return
+    try:
+        (letter, number) = data.split()
+        Q.append((letter, number, time.time(), address))
+    except:
+        traceback.print_exc()
 
 # Create a UDP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,20 +82,35 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('0.0.0.0', PORT))
 
 while True:
-    print >>sys.stderr, '\nwaiting to receive message'
-    data, address = sock.recvfrom(4096)
+    (r,w,x) = select.select((sock,),(),(),1)
 
-    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >>sys.stderr, data
+    if r:
+        (data, address) = sock.recvfrom(4096)
+        process(data, address)
 
-    (letter, number) = data.split()
+    if p:
+        if p.poll() is not None:
+            print(p.wait())
+            p = None
+            filename = None
+        else:
+            print(filename)
+    if Q:
+        if p:
+            print(Q)
+        else:
+            (letter, number, epoch, address) = Q.pop(0)
+            p = playGlob("/var/jukebox/%s/%s/*" % (letter, number))
+            if p:
+                (filename, p) = p
+    elif p:
+        print(filename)
+    else:
+        print('.')
 
-    try:
-        os.system("play /var/jukebox/%s/%s/*" % (letter, number))
-    except:
-        trace_back.print_exc()
-
-#    if data:
-#        sent = sock.sendto(data, address)
-#        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
-
+    with AtomicFile('/dev/shm/jukebox.json') as w:
+        w.write(json.dumps({'current': filename,
+                            'length' : len(Q),
+                            'queue'  : Q,
+                            },
+                           indent=4, sort_keys=1))
