6aa3b60a7c164eaafa44681ac3d280dc99682cef
Thomas Sprinkmeier <thomas.sprinkmeier@gmail.com>
2014-01-11 14:05:51 +0800

    POC of presenting sorted songs

--- jukebox-flask.py
+++ jukebox-flask.py
@@ -7,7 +7,16 @@ WSGI webapp using Flask
 from flask import Flask, redirect, render_template, url_for
 
 
+from flask.ext.wtf import Form
+
+from wtforms import TextField, validators
+
+import collections
+
 app = Flask(__name__)
+
+
+TEMPLATE = 'jukebox.html'
 
 
 @app.route('/')
@@ -15,6 +24,42 @@ def index():
     """Homepage"""
 
     return "Hello, linux.conf.au"
+
+
+class RegoForm(Form):
+    """A simple rego form"""
+
+    email = TextField('Email')
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def get_register():
+    """Handle the registration form"""
+
+    form = RegoForm()
+
+    form.letters = "ABCD"
+    form.songs = {'A': ["ABC","DEF","GHJ","JKL",],
+                  'B': [],
+                  'C': [],
+                  'D': ["xyz","stu"],
+                  'E': ["lmn","opq"],
+                  }
+#    form.songs = ["ABC","DEF","GHJ","JKL",]
+
+    form.songs = collections.OrderedDict()
+
+    form.songs['A'] = ["ABC","DEF","GHJ","JKL",]
+    form.songs['B'] = []
+    form.songs['C'] = []
+    form.songs['D'] = ["xyz","stu"]
+    form.songs['E'] = ["lmn","opq"]
+
+
+    if form.validate_on_submit():
+        return "Success"
+
+    return render_template(TEMPLATE, form=form)
 
 
 @app.route('/a-redirect')
@@ -28,8 +73,9 @@ def a_redirect():
 def a_template():
     """Render a template"""
 
-    return render_template('template.html')
+    return render_template(TEMPLATE)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'THIS IS REALLY SECRET (which should _NOT_ be stored in the code!!)'
     app.run(debug=True)
